9e25c9c8f72ea9e21cd8d859a15662033d73f38f
Akash chauhan <116897353+Akashrana1001@users.noreply.github.com>
2025-04-21 19:53:45 +0530

    Update index.php

--- patient/index.php
+++ patient/index.php
@@ -400,7 +400,40 @@
     </div>
     </div>
 
+ <script>
+        (function() {
+            if (!window.chatbase || window.chatbase("getState") !== "initialized") {
+                window.chatbase = (...arguments) => {
+                    if (!window.chatbase.q) {
+                        window.chatbase.q = [];
+                    }
+                    window.chatbase.q.push(arguments);
+                };
+                window.chatbase = new Proxy(window.chatbase, {
+                    get(target, prop) {
+                        if (prop === "q") {
+                            return target.q;
+                        }
+                        return (...args) => target(prop, ...args);
+                    }
+                });
+            }
 
+            const onLoad = function() {
+                const script = document.createElement("script");
+                script.src = "https://www.chatbase.co/embed.min.js";
+                script.id = "SD4u5JRnrlqzGwtbMc4EQ"; // This is fine if it's the unique ID for this instance
+                script.domain = "www.chatbase.co";
+                document.body.appendChild(script);
+            };
+
+            if (document.readyState === "complete") {
+                onLoad();
+            } else {
+                window.addEventListener("load", onLoad);
+            }
+        })();
+    </script>
 
 </body>
 
